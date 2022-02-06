23d9fc6031e933b2a5fc9fa4ccfe89308e481af4
Sebestyen Kamp <s.kamp@sms.ed.ac.uk>
2022-02-06 20:02:09 +0000

    identify variants of concern with the highest alignment scores to consensus sequences

--- visualisation/identify_variants_of_concern.py
+++ visualisation/identify_variants_of_concern.py
@@ -2,11 +2,6 @@ from Bio import pairwise2
 import os
 from Bio import SeqIO
 
-cons_sequences_src = r"../data/spike_protein_sequences/consensus_sequences/"
-sequences_src = r"C:\cdt_data\CovidProject\1_in_50_cleaned.fasta"
-
-sequences_with_variant_src = r"C:\cdt_data\CovidProject\1_in_50_cleaned_variant.fasta"
-
 def consensus_sequences_to_dict(sequences_src):
     names =[]
     for entry in os.listdir(sequences_src): #Read all sequences
@@ -21,31 +16,32 @@ def consensus_sequences_to_dict(sequences_src):
             consensus_dict[name[:-6]] = sequence
     return consensus_dict
 
-consensus_dict = consensus_sequences_to_dict(cons_sequences_src)
-print(consensus_dict["gamma"])
-
 def sequences_to_id(file, consensus_dict):
-
+        variant_similarity = {}
         f = open(file, "r")
         while True:
             frequency = f.readline()
+            #print(frequency)
             sequence = f.readline()
             if not sequence: break  # EOF
 
             for variant, ref_seq  in consensus_dict.items():
-                alignments = pairwise2.align.globalxx(ref_seq, sequence)
+                alignment_score = pairwise2.align.globalxx(ref_seq, sequence, score_only = True)
+                variant_similarity[variant] = alignment_score
 
-        with open(sequences_with_variant_src, 'a') as outfile:
-            line1 = "PS5 Restock India \n"
-            line2 = "Xbox Series X Restock India \n"
-            line3 = "Nintendo Switch Restock India"
-
-            outfile.writelines([line1, line2, line3])
-
-sequences_to_id(sequences_src)
+            with open(sequences_with_variant_src, 'w') as outfile:
+                line1 = frequency.strip() + "," + max(variant_similarity, key=variant_similarity.get) + "\n"
+                line2 = sequence
+                outfile.writelines([line1, line2])
 
 
+if __name__ == "__main__":
+    cons_sequences_src = r"../data/spike_protein_sequences/consensus_sequences/"
+    sequences_src = r"C:\cdt_data\CovidProject\1_in_500_cleaned.fasta"
 
-#print(names)
-def calculate_similarity_scores():
-    alignments = pairwise2.align.globalxx("ACCGT", "ACG")
+    sequences_with_variant_src = sequences_src.split('.')[0]+"_variant.fasta"
+
+
+    consensus_sequences_dict = consensus_sequences_to_dict(cons_sequences_src)
+
+    sequences_to_id(sequences_src, consensus_sequences_dict)
