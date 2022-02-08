bceff166edf010a7ce7a3b116a0f7e831ec53290
Dom Phillips <dom.phillips97@gmail.com>
2022-02-08 16:35:42 +0000

    fixed epitope mask alignment

--- visualisation/epitope_mask.py
+++ visualisation/epitope_mask.py
@@ -13,7 +13,7 @@ def get_fasta_sequence_from_file(fasta_file):
     number_of_sequences_found = 0
     sequence = None
     for sequence_entry in SeqIO.parse(fasta_file, "fasta"):
-        _, sequence = sequence_entry.id, str(sequence_entry.seq)
+        sequence = str(sequence_entry.seq)
         number_of_sequences_found += 1
 
     if number_of_sequences_found != 1:
@@ -85,15 +85,57 @@ def align_sequences(sequence1, sequence2):
     return sequence1_aligned, sequence2_aligned
 
 
+def adjust_epitope_mask_to_aligned_database(reference_sequence, epitope_mask, aligned_fasta_database_file):
+    """
+    Imputes missing entries into an epitope mask if "-"s appear in the aligned reference sequence in
+    aligned_fasta_database_file.
+    """
+    for sequence_entry in SeqIO.parse(aligned_fasta_database_file, "fasta"):
+        sequence = str(sequence_entry.seq)
+        stripped_sequence = sequence.replace('-', '')
+
+        # if the sequence found is a subsequence of the reference then it is a match
+        if stripped_sequence in reference_sequence:
+            refined_epitope_mask = np.zeros(len(sequence))
+            indices_of_hyphens = np.char.find(sequence, '-')
+
+            # extend length of epitope mask by number of hyphens found
+            for index, _ in enumerate(sequence):
+                number_of_hyphens_passed = len(indices_of_hyphens[indices_of_hyphens < index])
+                if index in indices_of_hyphens:
+                    # impute missing entries with the average values of adjacent entries
+                    refined_epitope_mask[index] = (epitope_mask[index-number_of_hyphens_passed-1]+
+                                                   epitope_mask[index-number_of_hyphens_passed])/2
+                else:
+                    refined_epitope_mask[index] = epitope_mask[index-number_of_hyphens_passed]
+            break
+
+    try:
+        return refined_epitope_mask
+    except:
+        raise Exception('The reference sequence does not appear in the provided fasta file.')
+
+
 def get_epitope_mask(fasta_file,
                      dssp_file,
                      data_directory,
+                     aligned_file,
                      normalise=True,
                      threshold=None):
-    # TODO: complete description
     """
     Pairwise sequence aligns a reference fasta sequence to a dssp sequence and returns
     a solvent accessibility vector for the fasta sequence.
+
+    :param fasta_file: The fasta file of the reference sequence.
+    :param dssp_file:  The dssp file of the reference sequence (generated from a PDB file)
+    :param data_directory: The relative path to the directory containing the fasta file and dssp file.
+    :param aligned_file: The fasta database of aligned sequences which the mask will be applied to. Needed to make sure
+                         that the size of the epitope mask vector is expanded to the length of the sequence-aligned
+                         reference sequence.
+    :param normalise: If true then normalises the epitope mask relative to the size of its maximum entry.
+    :param threshold: [Optional] A float between 0 and 1. Values below this relative size are set to zero,
+                       values above are set to 1. I.e. this makes the epitope mask a binary mask.
+    :return: epitope mask
     """
     reference_sequence = get_fasta_sequence_from_file(data_directory+fasta_file)
     dssp_solvent_accessibility = extract_solvent_accessibility_from_dssp_file(data_directory+dssp_file)
@@ -104,6 +146,7 @@ def get_epitope_mask(fasta_file,
     position_in_dssp_sequence = 0
     epitope_mask = []
 
+    # converts a dssp solvent accessibility vector into a vector defined relative to the original fasta sequence
     for index, dssp_amino_acid in enumerate(aligned_dssp_sequence):
         if aligned_reference_sequence[index] == "-":
             continue
@@ -115,6 +158,7 @@ def get_epitope_mask(fasta_file,
             else:
                 epitope_mask.append(0)
 
+    # apply optional post-processing to the epitope mask
     epitope_mask = np.array(epitope_mask)
     if threshold is not None:
         assert 1 > threshold > 0, "threshold must be a float between 0 and 1"
@@ -124,7 +168,11 @@ def get_epitope_mask(fasta_file,
     if normalise:
         epitope_mask /= np.max(epitope_mask)
 
+    # adjust epitope mask so that it aligns to a given database
+    epitope_mask = adjust_epitope_mask_to_aligned_database(reference_sequence, epitope_mask, aligned_file)
+
     return epitope_mask, reference_sequence
+
 
 
 if __name__ == "__main__":
@@ -133,9 +181,8 @@ if __name__ == "__main__":
     spike_sequence_src = r"../data/spike_protein_pdb/rcsb_pdb_7N1U.fasta"
     spike_dssp_src = r"../data/spike_protein_pdb/7n1u.dssp"
 
-    epitope_mask, reference_sequence = get_epitope_mask(fasta_file='rcsb_pdb_7N1U.fasta', dssp_file='7n1u.dssp', data_directory=data_dir)
-    print(epitope_mask)
-
-    # TODO: function that reads in aligned sequence file, finds the reference sequence in that file and
-    #  adjusts the epitope mask accordingly
+    epitope_mask, reference_sequence = get_epitope_mask(fasta_file='reference_spike.fasta',
+                                                        dssp_file='reference_spike.dssp',
+                                                        aligned_file='../data/spike_protein_sequences/1_in_500_cleaned_aligned.afa',
+                                                        data_directory=data_dir)
     
