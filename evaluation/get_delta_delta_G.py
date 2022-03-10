ab13f6bd9f7b630037f9510b38da08648055548f
Sebestyen Kamp <s.kamp@sms.ed.ac.uk>
2022-03-10 15:55:21 +0000

    ddgun and ddg results

--- evaluation/get_delta_delta_G.py
+++ evaluation/get_delta_delta_G.py
@@ -17,12 +17,15 @@ def get_delta_delta_g_from_file(filename):
 if __name__ == "__main__":
 
     ddg_sum_dict={}
-
-    for seq_number in range(90):
-        filename = './synthetic_point_mutations/'+ str(seq_number)+'.txt'
+    #foldername = 'synthetic_point_mutations'
+    foldername = 'synthetic2_point_mutations'
+    #foldername = '11gram_point_mutations'
+    #foldername = 'random75_point_mutations'
+    for seq_number in range(117):
+        filename = './' + foldername +'/'+ str(seq_number)+'.txt'
         ddg_sum = get_delta_delta_g_from_file(filename)
         ddg_sum_dict[str(seq_number)] = ddg_sum
     print(ddg_sum_dict)
     ddg_sum_df = pd.DataFrame.from_dict(ddg_sum_dict,orient='index', columns=['ddg_sum'])
     print(ddg_sum_df)
-    ddg_sum_df.to_csv('ddg_sum_table.csv', index=True)
+    ddg_sum_df.to_csv(foldername + '_ddg_sum_table.csv', index=True)
